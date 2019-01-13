25ef767ce968c4f1ed7b773da244fba979e3df97
Josh Dolitsky <jdolitsky@gmail.com>
2019-01-13 17:32:54 -0600

    fix storage.go
    Signed-off-by: Josh Dolitsky <jdolitsky@gmail.com>

--- vendor/github.com/chartmuseum/storage/storage.go
+++ vendor/github.com/chartmuseum/storage/storage.go
@@ -1,20 +1,7 @@
-/*
-Copyright The Helm Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package storage
+
+// This file is a replacement for vendor/github.com/chartmuseum/storage/storage.go
+// with a modified "objectPathIsInvalid" method (allows for recursion)
 
 import (
 	"fmt"
@@ -100,5 +87,5 @@ func removePrefixFromObjectPath(prefix string, path string) string {
 }
 
 func objectPathIsInvalid(path string) bool {
-	return strings.Contains(path, "/") || path == ""
+	return path == ""
 }
